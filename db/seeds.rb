3a643b732e2e54a65996a043395607bb3b3cab20
viniciusfer01 <business.fernandes22@gmail.com>
2025-08-05 20:32:29 -0300

    cleanup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 require_relative '../app/services/log_parser'
 
 puts "Limpando o banco de dados..."
@@ -19,7 +9,6 @@ GameEvent.delete_all
 Player.delete_all
 
 puts "Processando o log do jogo..."
-# Coloque o seu arquivo de log na raiz do projeto ou em um diretório como `lib/logs`
 LogParser.parse('game_log_large.txt')
 
 puts "Carga de dados finalizada!"
