cb8b4b17fdff074507f53a3c7b6130432e7c7fc7
viniciusfer01 <business.fernandes22@gmail.com>
2025-08-05 20:05:20 -0300

    #2 - Parse gamelog and #3 - save to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require_relative '../app/services/log_parser'
+
+puts "Limpando o banco de dados..."
+Kill.delete_all
+ItemPickup.delete_all
+QuestCompletion.delete_all
+BossKill.delete_all
+GameEvent.delete_all
+Player.delete_all
+
+puts "Processando o log do jogo..."
+# Coloque o seu arquivo de log na raiz do projeto ou em um diretório como `lib/logs`
+LogParser.parse('game_log_large.txt')
+
+puts "Carga de dados finalizada!"
