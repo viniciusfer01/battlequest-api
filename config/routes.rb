86236f9acda7d685c89c58dba04dd191f8fbbbbe
viniciusfer01 <business.fernandes22@gmail.com>
2025-08-06 13:27:40 -0300

    fix exposed resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "items/top"
-      get "events/index"
+      resources :events, only: [ :index ]
       get "leaderboard/index"
       resources :players, only: [ :index ] do
         member do
