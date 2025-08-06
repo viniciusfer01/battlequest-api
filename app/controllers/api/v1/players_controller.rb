60219991b7e80d2935b2b4135954e5805ad8b305
viniciusfer01 <business.fernandes22@gmail.com>
2025-08-05 22:29:13 -0300

    #5 - add player details endpoint

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::PlayersController < ApplicationController
     @players = Player.all
     render json: @players, status: :ok
   end
+
+  def stats
+    player = Player.find(params[:id])
+    stats = {
+      player_id: player.id,
+      name: player.name,
+      score: player.score,
+      kills: Kill.where(killer_id: player.id).count,
+      deaths: Kill.where(victim_id: player.id).count,
+      items_collected: ItemPickup.where(player_id: player.id).sum(:quantity),
+      quests_completed: QuestCompletion.where(player_id: player.id).count
+    }
+
+    render json: stats
+  end
 end
--- test/controllers/api/v1/players_controller_test.rb
+++ test/controllers/api/v1/players_controller_test.rb
@@ -2,17 +2,36 @@ require "test_helper"
 
 class Api::V1::PlayersControllerTest < ActionDispatch::IntegrationTest
   test "should show players" do
-    player1 = players(:one)
-    player2 = players(:two)
+    player1 = players(:zezin)
+    player2 = players(:chiquin)
 
     get api_v1_players_url
 
     assert_response :success
-    assert_equal player1.id, JSON.parse(response.body)[1]["id"]
-    assert_equal player1.name, JSON.parse(response.body)[1]["name"]
-    assert_equal player1.score, JSON.parse(response.body)[1]["score"]
+    assert_equal player1.id, JSON.parse(response.body)[3]["id"]
+    assert_equal player1.name, JSON.parse(response.body)[3]["name"]
+    assert_equal player1.score, JSON.parse(response.body)[3]["score"]
     assert_equal player2.id, JSON.parse(response.body)[0]["id"]
     assert_equal player2.name, JSON.parse(response.body)[0]["name"]
     assert_equal player2.score, JSON.parse(response.body)[0]["score"]
+  end
+
+  test "should show player stats" do
+    player = players(:zezin)
+    kills(:one)
+    item_pickups(:one)
+    quest_completions(:one)
+    boss_kills(:one)
+
+    get stats_api_v1_player_url(player)
+
+    assert_response :success
+    assert_equal player.id, JSON.parse(response.body)["player_id"]
+    assert_equal player.name, JSON.parse(response.body)["name"]
+    assert_equal player.score, JSON.parse(response.body)["score"]
+    assert_equal 1, JSON.parse(response.body)["kills"]
+    assert_equal 0, JSON.parse(response.body)["deaths"]
+    assert_equal 16, JSON.parse(response.body)["items_collected"]
+    assert_equal 1, JSON.parse(response.body)["quests_completed"]
   end
 end
